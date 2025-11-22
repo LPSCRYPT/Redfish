c571cb23f6c7609250a6660cd1bde6ce601404f0
cesar <cesar23augusto16@gmail.com>
2025-11-22 15:03:34 -0600

    Eda terminado

--- model/EDA.py
+++ model/EDA.py
@@ -4,7 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
-
+import os
 
 DATA_PATH = 'data/transaction_dataset.csv'
 
@@ -19,6 +19,11 @@ def print_info(df):
     print(df.head())
 
 
+def save_image(fig, filename):
+    os.makedirs('images', exist_ok=True)
+    fig.savefig(os.path.join('images', filename))
+
+
 def inspect_flag_distribution(df, flag_column):
     # Inspect target distribution
     print(df[flag_column].value_counts())
@@ -28,9 +33,10 @@ def inspect_flag_distribution(df, flag_column):
     plt.pie(x = df[flag_column].value_counts(), autopct='%.2f%%', explode=[0.02]*2, labels=labels, pctdistance=0.5, textprops={'fontsize': 14}, colors = colors)
     plt.title('Target distribution')
     plt.show()
+    save_image(pie, "flag_distribution.png")
 
 
-def correlation_heatmap(df):
+def correlation_heatmap(df, annotation=False):
     # Correlation matrix
     numericals = df.select_dtypes(include=['float','int']).columns
     corr = df[numericals].corr()
@@ -38,16 +44,18 @@ def correlation_heatmap(df):
     mask = np.zeros_like(corr)
     mask[np.triu_indices_from(mask)]=True
     with sns.axes_style('white'):
-        plt.subplots(figsize=(18,10))
-        sns.heatmap(corr,  mask=mask, annot=True, cmap='coolwarm', center=0, square=True)
+        fig, ax =plt.subplots(figsize=(20,12))
+        sns.heatmap(corr,  mask=mask, annot=False, cmap='coolwarm', center=0, square=True)
+        plt.title('Correlation Heatmap', fontsize=10)
+        plt.show()
+        save_image(fig, "correlation_heatmap.png")
 
 
 def main():
     df = pd.read_csv(DATA_PATH)
     print_info(df)
     inspect_flag_distribution(df, 'FLAG')
-    correlation_heatmap(df)
-
+    correlation_heatmap(df, annotation=False)
 
 if __name__ == "__main__":
     main()
